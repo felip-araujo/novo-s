0d8ca98ba724a8082c2bc50441950f8c6a3cf526
felip-araujo <felipearaujoexecutive@gmail.com>
2025-01-14 11:18:27 -0400

    hero e projects

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: false,
-  
+
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent   {
+
+  @ViewChild('mobileMenu') mobileMenu!: ElementRef;
+
+  // Método para abrir o menu
+  openMenu(): void {
+    this.mobileMenu.nativeElement.classList.remove('hidden');
+  }
+
+  // Método para fechar o menu
+  closeMenu(): void {
+    this.mobileMenu.nativeElement.classList.add('hidden');
+  }
+
+  // Fecha o menu ao clicar fora
+  closeMenuOnClickOutside(event: Event): void {
+    if (event.target === this.mobileMenu.nativeElement) {
+      this.mobileMenu.nativeElement.classList.add('hidden');
+    }
+  }
 
 }
